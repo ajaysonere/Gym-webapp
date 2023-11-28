3f597b1bf8473eaee6ae6b036dbed1f40c9515fc
ajay sonere <ajaysonere786@gmail.com>
2023-11-28 12:49:44 +0530

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,36 @@ import { Link , NavLink } from 'react-router-dom';
 import logo from '../images/logo.png';
 import { links } from '../data';
 import { FaBars } from "react-icons/fa6";
+import { MdOutlineClose } from "react-icons/md";
+
+import { useState } from 'react';
 
 const Navbar = ()=>{
-    return(
-       <nav>
-          <div className='container  nav__container'>
-              <Link to='/' className='logo'> 
-                 <img src={logo} alt='Logo'></img>
-              </Link>
-              <ul>
-                {
-                    links.map(({name , path}, index )=>{
-                        return(
-                            <li>
-                                <NavLink to={path} className="nav__links">{name}</NavLink>
-                            </li>
-                        )
-                    })
-                }
-              </ul>
-              <button className='nav__toggle-btn'>
-                <FaBars/>
-              </button>
-          </div>
-       </nav>
+
+    const [showNavigation , setShowNavigation] = useState(false);
+
+    return (
+      <nav>
+        <div className="container  nav__container">
+          <Link to="/" className="logo">
+            <img src={logo} alt="Logo"></img>
+          </Link>
+          <ul className={`nav__links ${showNavigation ? 'show-nav' :'hide-nav'}`}>
+            {links.map(({ name, path }, index) => {
+              return (
+                <li key={index}>
+                  <NavLink to={path} className={({isActive}) => isActive ? "active-nav" : ''} >{name}  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+          <button className="nav__toggle-btn" onClick={()=>setShowNavigation(!showNavigation)}>
+            {
+                showNavigation ? <MdOutlineClose/> : <FaBars />
+            }
+          </button>
+        </div>
+      </nav>
     );
 };
 

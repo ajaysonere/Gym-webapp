96773f149e97a7570ba12dffda2ad9a09327f10f
ajay sonere <ajaysonere786@gmail.com>
2023-11-28 10:14:26 +0530

    data file and routers added in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+// pages 
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Gallery from './pages/Gallery/Gallery';
+import Plans from './pages/Plans/Plans';
+import Trainers from './pages/Trainers/Trainers';
+import Notfound from './pages/Notfound/Notfound';
+
+// components
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
-      <h2>hello ji ? </h2>
+    <BrowserRouter>
+       <Navbar/>
+       <Routes >
+         <Route></Route>
+       </Routes>
+    </BrowserRouter>
   );
 };
 

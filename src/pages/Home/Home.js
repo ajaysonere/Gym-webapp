d38869371212e2a4b4a8ed8eafe838cf54eecc0d
ajay sonere <ajaysonere786@gmail.com>
2023-11-28 19:28:31 +0530

    programs component completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import Mainheader from '../../components/Mainheader';
+import Programs from '../../components/Programs';
 import './home.css';
 
 const Home = ()=>{
     return(
        <div >
             <Mainheader/>
+            <Programs />
        </div>
     );
 };

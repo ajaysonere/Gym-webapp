9bb6f5c2d78fbb2ea5e57e6783ed6c115fb31943
ajay sonere <ajaysonere786@gmail.com>
2023-11-28 15:39:30 +0530

    section header added

---
+++ src/components/Sectionheader.js
@@ -0,0 +1,11 @@
+
+const Sectionheader = ({icons , title , className})=>{
+    return (
+      <div className={`section__head ${className}`}>
+        <span> {icons} </span>
+        <h2> {title} </h2>
+      </div>
+    );
+};
+
+export default Sectionheader;

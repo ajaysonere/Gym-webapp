9d15866994e4bd99fc9be732e98c99704ac39500
ajay sonere <ajaysonere786@gmail.com>
2023-11-30 16:02:23 +0530

    responsiveness

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,10 +1,16 @@
+import headerImage from '../../images/header_bg_3.jpg';
+import Header from '../../components/Header';
+
 import './gallery.css';
 
 const Gallery = () => {
-    return(
-       <div> 
-           <h1> Gallery </h1>
-       </div>
+    return (
+      <div>
+        <Header title="Our Gallery" icon={headerImage}>
+          Proin efficitur mauris at quam facilisis, id efficitur felis rhoncus.
+          Curabitur sodales quam sit amet tortor luctus.
+        </Header>
+      </div>
     );
 };
 

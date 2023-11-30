b9837ccf08f50c14dcbdd056e3f0bd73765a2da3
ajay sonere <ajaysonere786@gmail.com>
2023-11-30 22:09:33 +0530

    trainer page and web completed

--- src/data.js
+++ src/data.js
@@ -280,41 +280,41 @@ export const trainers = [
         image: Trainer1,
         name: 'John Doe',
         job: 'Aerobic Trainer',
-        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+        socials: ['https://gmail.com/', 'https://github.com/', 'https://twitter.com/', 'https://linkedin.com/']
     },
     {
         id: 2,
         image: Trainer2,
         name: 'Daniel vinyo',
         job: 'Speed Trainer',
-        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+        socials: ['https://gmail.com/', 'https://github.com/', 'https://twitter.com/', 'https://linkedin.com/']
     },
     {
         id: 3,
         image: Trainer3,
         name: 'Edem Quist',
         job: 'Flexibility Trainer',
-        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+        socials: ['https://gmail.com/', 'https://github.com/', 'https://twitter.com/', 'https://linkedin.com/']
     },
     {
         id: 4,
         image: Trainer4,
         name: 'Shatta Wale',
         job: 'Body Composition Trainer',
-        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+        socials: ['https://gmail.com/', 'https://github.com/', 'https://twitter.com/', 'https://linkedin.com/']
     },
     {
         id: 5,
         image: Trainer5,
         name: 'Diana Ayi',
         job: 'Circuit Trainer',
-        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+        socials: ['https://gmail.com/', 'https://github.com/', 'https://twitter.com/', 'https://linkedin.com/']
     },
     {
         id: 6,
         image: Trainer6,
         name: 'Wayne Carter',
         job: 'Physical Intelligence Trainer',
-        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+        socials: ['https://gmail.com/', 'https://github.com/', 'https://twitter.com/', 'https://linkedin.com/']
     }
 ]
--- src/pages/Trainers/Trainers.js
+++ src/pages/Trainers/Trainers.js
@@ -1,9 +1,37 @@
 import './trainers.css';
+import headerImage from '../../images/header_bg_5.jpg';
+import Header from '../../components/Header';
+import {trainers} from '../../data';
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import Trainer from '../../components/Trainer';
+
 
 const Trainers = () =>{
     return(
         <div>
-             <h1>Trainers</h1>
+             <Header title="Our Trainers" icon={headerImage}>
+                Proin efficitur mauris at quam facilisis, id efficitur felis
+                rhoncus. Curabitur sodales quam sit amet tortor
+             </Header>
+             <section className='trainers'>
+                <div className='container trainers__container'>
+                     {
+                        trainers.map(({id , image ,name , job , socials})=>{
+                            return  <Trainer key={id} image={image} name={name} job={job} socials={
+                                [
+                                    {icon:<MdEmail /> , link: socials[0]},
+                                    {icon:<FaGithub /> , link: socials[1]},
+                                    {icon:<AiOutlineTwitter /> , link: socials[2]},
+                                    {icon:<FaLinkedin /> , link: socials[3]}   
+                                ]
+                            } />
+                        })
+                     }
+                </div>
+             </section>
         </div>
     );
 };

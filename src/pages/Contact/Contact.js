2a3fd7fdb0f71a0caad5ee68cd8d74af8f5a5d99
ajay sonere <ajaysonere786@gmail.com>
2023-11-30 15:27:28 +0530

    contact page completed

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,32 @@
+import Header from '../../components/Header';
+import headerImage from '../../images/header_bg_2.jpg';
+import {MdEmail} from 'react-icons/md';
+import { FaGithub } from "react-icons/fa";
+import {IoLogoWhatsapp} from 'react-icons/io';
+
 import './contact.css';
 
 const Contact = () => {
     return (
         <div>
-            <h1>contact</h1>
+            <Header title="Get in Touch" icon={headerImage} >
+                 we are on mission to aware people about the health <br /> and motivate them for workout. 
+            </Header>
+            <section className='contact'>
+                <div className='container contact__container'>
+                    <div className='contact__wrapper'>
+                        <a href='mailto:ajaysonere786@gmail.com' target='_blank' rel="noreferrer noopener">
+                            <MdEmail />
+                        </a>
+                        <a href="https://github.com/ajaysonere" target='_blank' rel="noreferrer noopener">
+                            <FaGithub />
+                        </a>
+                        <a href='https://wa.me/+919977449230' target='_blank' rel="noreferrer noopener">
+                            <IoLogoWhatsapp />
+                        </a>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
